e65d9ccb8eeec365ccd50498ccb724131e0775ed
Dixon <villandixon@yahoo.com>
2020-09-23 22:04:33 +0800

    completed instagram features

--- src/Post.js
+++ src/Post.js
@@ -59,25 +59,26 @@ function Post({postId, user, username, caption, imageUrl}) {
                 }
             </div>
 
+            {user && (
+                <form className="post__commentBox">
+                    <input 
+                    className="post__input"
+                    type="text"
+                    placeholder="Add a comment..."
+                    value={comment}
+                    onChange={(e) => setComment(e.target.value)}
+                    />
 
-            <form className="post__commentBox">
-                <input 
-                className="post__input"
-                type="text"
-                placeholder="Add a comment..."
-                value={comment}
-                onChange={(e) => setComment(e.target.value)}
-                />
-
-                <button
-                    className="post__button"
-                    disabled={!comment}
-                    type="submit"
-                    onClick={postComment}
-                > 
-                    Post
-                </button>
-            </form>
+                    <button
+                        className="post__button"
+                        disabled={!comment}
+                        type="submit"
+                        onClick={postComment}
+                    > 
+                        Post
+                    </button>
+                </form>
+            )}
         </div>
     )
 }
